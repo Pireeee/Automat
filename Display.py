dba04c5ff7ce60253557252b2a03b103c4f287bc
Pire <p.clement01@orange.fr>
2022-03-11 14:37:45 +0100

    Diplay END
    j'ai fini le l'affichage du graph

--- Display.py
+++ Display.py
@@ -1,32 +1,33 @@
-from msilib.schema import Class
 import networkx as nx
 import matplotlib.pyplot as plt
 from JSON import JSON
 import json
-
+#cette partie nécéssite de
 
 class Display(): #il me reste: gérer état initial et final, les noms sur les fleches
 
     def __init__(self):
         self.list_tuple_nodes=[]
-        self.edge_labels={}
+        self.edge_labels=dict()
         self.tuple_edge=()
         self.initial=''
         self.finaux=[]
         self.transitions=dict()
+        self.layout=''
 
 
 
-    def generate_tuples_node(self): #créé une liste de 2-tuples à partir du JSON
+    def generate_tuples_node(self): #créé une liste de 2-tuples à partir du JSON et un dictionnaire avec comme clefs les 2-tuples
         self.mon_json = JSON('','')
         self.mon_json=  self.mon_json.lire_json()
+        self.layout= 'nx.'+self.mon_json["Layout"]+'_layout(G)'
         self.initial= self.mon_json["Etat initial"]
         self.finaux = self.mon_json["Etats finaux"]
         self.transitions=self.mon_json["Transitions"][0]
         for keys in self.transitions.keys(): #boucle qui sépare les clefs du dictionaire transition dans une liste
             liste = keys.split(',')
 
-            for i in range(len(liste)): #on parcour la nouvelle liste pour vérifier si les etats son initiaux
+            for i in range(len(liste)): #on parcours la nouvelle liste pour vérifier si les états son initiaux
                 if (liste[i]== self.initial):
                     liste[i]= "*" + liste[i]
 
@@ -34,32 +35,33 @@ class Display(): #il me reste: gérer état initial et final, les noms sur les f
                     if(liste[i] == self.finaux[j]):
                         liste[i] = self.finaux[j] + "->"
 
-            tuple_node = tuple(liste) #crée un tuple a partir de la liste
-            self.list_tuple_nodes.append(tuple_node) # crée une liste de 2-tuple 
-        #self.edge_labels ={
-        #    ('*q','B'):'a',
-        #    ('A*','C'):'b',
-        #    ('C','A*'):'c'
-        #}
+            tuple_node = tuple(liste) #crée un tuple a partir de la liste 
+            self.edge_labels[tuple_node]=self.transitions[keys] 
+            self.list_tuple_nodes.append(tuple_node)
+        print(self.layout)
 
-    #def generate_edge_labels(self): #doit créer un dictionaire avec comme clefs des 2-tuples
-
-        
-
+    
     def draw_graph(self): #génere le graphique 
         G = nx.DiGraph()
         G.add_edges_from(self.list_tuple_nodes) #nome les nodes et leurs affiliations
-        pos = nx.spring_layout(G)   #le layout dans lequel va se former le graph https://networkx.org/documentation/stable/reference/generated/networkx.drawing.layout.spring_layout.html
-        nx.draw_networkx_nodes(G, pos, node_size=1000)#place les nodes (cercles) et donne la taille
-        nx.draw_networkx_edges(G, pos, edgelist=G.edges(),edge_color='hotpink',connectionstyle="arc3,rad=0.15",arrows=True,arrowsize=10,width=2,node_size=1000) #place les fleches (defini couleur et le radius des arc)
-        nx.draw_networkx_labels(G, pos)
-        #dessine les noms des nodes
-        #nx.draw_networkx_edge_labels( #crée les noms nur les fleches
-        #    G,pos,
-        #    edge_labels=self.transitions,
-        #    font_color='hotpink',#couleur de la fleche
-        #    label_pos=0.7#espacement
-        #)
+        if (self.layout == 'shell'):#le layout dans lequel va se former le graph il y à le choix entre random,shell,spectral et par défault c'est spring
+            pos=nx.shell_layout(G)
+        elif (self.layout == 'random'):
+             pos=nx.random_layout(G)
+        elif (self.layout == 'spectral'):
+             pos=nx.spectral_layout(G)
+        else:
+            pos=nx.shell_layout(G)
+
+        nodes = nx.draw_networkx_nodes(G, pos, node_size=1000,node_color="#FFFFFF")#place les nodes (cercles) et donne la taille
+        nodes.set_edgecolor('black')
+        print(nodes)
+        nx.draw_networkx_edges(G, pos, edgelist=G.edges(),connectionstyle="arc3,rad=0.17",arrows=True,arrowsize=10,width=1.5,node_size=1000) #place les fleches (defini couleur et le radius des arc)
+        nx.draw_networkx_labels(G, pos) #place les noms dans les nodes
+        nx.draw_networkx_edge_labels( #crée les noms nur les fleches
+            G,pos,
+            edge_labels=self.edge_labels, #dictionaire
+            label_pos=0.7#espacement
+        )
         plt.axis('off')
         plt.show()
-        #
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 Automat
+voici le format du JSON
+    "Etats":liste de str ,
+    "Alphabet":liste de str,
+    "Transitions":dictionaire avec comme format <"etat_départ,etat_arrivé":"mot">,
+    "Etat initial":str,
+    "Etats finaux":liste de str ,
+    "Layout":str ("spring" par défaut)
+
+lors de l'affichage vous pouvez changer les layouts (dispostion des nodes) les disponibles sont :
+-random
+-spectral
+-shell
+-spring(defaut)
